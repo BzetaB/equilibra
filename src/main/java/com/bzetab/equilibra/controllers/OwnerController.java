6ef6940d582a2508d9280d014e5d7a4e14e565f6
bzetab <zevallosbenjamin100@gmail.com>
2026-02-02 14:01:14 -0500

    add Owner controller endpoints

---
+++ src/main/java/com/bzetab/equilibra/controllers/OwnerController.java
@@ -0,0 +1,58 @@
+package com.bzetab.equilibra.controllers;
+
+import com.bzetab.equilibra.common.ApiResponse;
+import com.bzetab.equilibra.dtos.requests.CreateOwnerRequest;
+import com.bzetab.equilibra.models.Owner;
+import com.bzetab.equilibra.services.OwnerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/owner")
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService){
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<Owner>> getOwnerByUniqueCode(@RequestParam("uniqueCode") String uniqueCode){
+        return ResponseEntity.ok(
+                ApiResponse.<Owner>builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.OK.value())
+                        .message("Owner found successfully")
+                        .data(ownerService.getOwnerByUniqueCode(uniqueCode))
+                        .build()
+        );
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<Owner>> createOwner(@RequestBody CreateOwnerRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.<Owner>builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.CREATED.value())
+                        .message("Owner created successfully")
+                        .data(ownerService.createOwner(request.getUniqueCode()))
+                        .build()
+        );
+    }
+
+    @PutMapping()
+    public ResponseEntity<ApiResponse<Owner>> updateStatusOwner(@RequestParam("uniqueCode") String uniqueCode){
+        return ResponseEntity.ok(
+                ApiResponse.<Owner>builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.OK.value())
+                        .message("Owner status updated successfully")
+                        .data(ownerService.updatedStatusOwner(uniqueCode))
+                        .build()
+        );
+    }
+}
